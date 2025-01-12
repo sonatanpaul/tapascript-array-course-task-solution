23ac712c72290c1ba274a96e45ff8bb636ca641f
sonatanpaul <iamsonatanpaul@gmail.com>
2025-01-12 23:24:22 +0600

    task 3 problem solve

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ const number = new Array(10);
 console.log(number);
 // Output: (10) [empty × 10]
 
+// T-003: Create an array of 6 elements using the Array literals and access the fourth element in the array using its length property.
+const city = ["Dhaka", "Gazipur", "Mirpur", "Kolkata", "Uttara", "Badda"];
+const cityLength = city.length;
+console.log(cityLength); //output : 6
+const favoriteCity = city[cityLength - 2];
+console.log(favoriteCity); // output : Uttara
