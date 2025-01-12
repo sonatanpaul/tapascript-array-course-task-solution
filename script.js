863e49c4460cc316baf883476e83d3c2b05de7ca
sonatanpaul <iamsonatanpaul@gmail.com>
2025-01-13 01:47:27 +0600

    task 5 and 6 problem solve

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ const cityLength = city.length;
 console.log(cityLength); //output : 6
 const favoriteCity = city[cityLength - 2];
 console.log(favoriteCity); // output : Uttara
+
+// T-004: Use the for loop on the above array to print elements in the odd index.
+const newCity = ["Dhaka", "Gazipur", "Mirpur", "Kolkata", "Uttara", "Badda"];
+
+for (let i = 1; i < newCity.length; i += 2) {
+  console.log(newCity[i]);
+}
+// Output : Gazipur  Kolkata Badda
+
+// T-005: Add one element at the front and the end of an array.
+// add last element of array
+const friend = ["Anik", "Asraful", "Rayhan"];
+friend.push("Akash");
+console.log(friend);
+// output : ['Anik', 'Asraful', 'Rayhan', 'Akash']
+
+// add first element of array
+const bestFriend = ["Anik", "Rayhan"];
+bestFriend.unshift("Asraful");
+console.log(bestFriend);
+// output:  ['Asraful', 'Anik', 'Rayhan']
+
+// T-006: Remove an element from the front and the end of an array.
+const removeLastElement = [1, 2, 3, 5, 6];
+removeLastElement.pop();
+console.log(removeLastElement);
+// Output : [1, 2, 3, 5]
+
+const removeFirstElement = [1, 2, 3, 4, 5, 6, 7];
+removeFirstElement.shift();
+console.log(removeFirstElement);
+// Output : [2, 3, 4, 5, 6, 7]
