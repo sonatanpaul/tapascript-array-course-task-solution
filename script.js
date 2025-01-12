a88eaf939c5872baeca0b7935cf8411f3178fff1
sonatanpaul <iamsonatanpaul@gmail.com>
2025-01-12 23:17:47 +0600

    task 2 solution done

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // T-001: Create an array of 5 elements using the Array Constructor.
 const person = new Array("Anik", "Rayhan", "Akash", "Asraful", "Bikash");
 console.log(person);
+// Output : ['Anik', 'Rayhan', 'Akash', 'Asraful', 'Bikash']
+
+// T-002: Create an array of 3 empty slots.
+const number = new Array(10);
+console.log(number);
+// Output: (10) [empty × 10]
+
